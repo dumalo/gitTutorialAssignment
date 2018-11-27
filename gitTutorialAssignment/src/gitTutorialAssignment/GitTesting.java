645431e0c682dcc6eab3b93b063ff4256765a2be
Duane Malone <duane.malone@gmai.com>
2018-11-26 20:07:50 -0400

    GitTest commited

---
+++ gitTutorialAssignment/src/gitTutorialAssignment/GitTesting.java
@@ -0,0 +1,15 @@
+package gitTutorialAssignment;
+
+public class GitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+
+	}
+	
+	public int addition (int a, int b) {
+		return a+b;
+	}
+
+}
